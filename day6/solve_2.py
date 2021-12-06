ebd546137cd695687db4f1892b6fc74898512b96
Meinel, Michael <michael.meinel@dlr.de>
2021-12-06 08:32:11 +0100

    Remove debug prints

--- day6/solve_2.py
+++ day6/solve_2.py
@@ -4,7 +4,6 @@ with open('input', 'r') as input_file:
         fish_pop[ctr] += 1
 
 for _ in range(256):
-    print(fish_pop)
     fish_pop[7] += fish_pop[0]
     fish_pop = fish_pop[1:] + fish_pop[:1]
 
